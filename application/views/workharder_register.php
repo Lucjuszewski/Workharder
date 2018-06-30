ecce85755307b8a223b59649c10e965371c9cd9e
Lucjuszewski <kosiorek_michal@o2.pl>
2018-06-30 22:45:51 +0200

    Menu logowania przeniesione również do podstrony z rejestracją ale ma to problemy | Login window moved too to register site but need fix

--- application/views/workharder_register.php
+++ application/views/workharder_register.php
@@ -24,10 +24,53 @@ if($isRegister) {
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src='https://www.google.com/recaptcha/api.js'></script>
 </head>
-<body style="height:100%;">
-   <div class="container-fluid x" style="height:100%;">
-     
-      <div class="row navbar">
+<body style="height:100%;width:100%;">
+  <script
+  src="https://code.jquery.com/jquery-3.3.1.js"
+  integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
+  crossorigin="anonymous"></script>
+<script>
+    $(function(){
+    $('.btn-login').on('click',function() {
+        $( ".dark-layer" ).show( 400, function() {
+    // Animation complete.
+        });
+    });
+        $('.hyperlink').on('click',function() {
+               $( ".dark-layer" ).hide( 400, function() {
+    // Animation complete.
+        });                          
+        });
+    });
+</script>
+   <div class="container-fluid x" style="height:100%; width:100%;padding-right: 0px; padding-left: 0px;">
+     <div class="dark-layer" style="background-color:#000000; 
+    position:absolute; 
+    height:100%; 
+    width: 100%;
+    z-index:1; opacity: 0.88; padding: 0 0 ; display:none;">
+    <div class="close-icon-container d-flex  justify-content-end"  style=" width:100%; margin-top:20px; padding-right:40px; "><div class="x-icon" style=" position:fixed; z-index:1;"><a href="####wyjscie" style="font-size:40px;" class="hyperlink" >X</a></div></div>
+    <div class="d-flex justify-content-center align-items-center" style="height:100%; width:100%; position:fixed;"><!--   Okno logowania  POCZĄTEK   -->
+       
+        <div class="bordersquare x row" style="width:700px;">
+        <div class="login-window col-5" style="border-right-style: dotted;  padding-right:35px; float:left;">
+        <form action="http://[::1]/codeigniter/workharder/register" method="post" accept-charset="utf-8" style="width:100%;">
+        <div class="full-width d-flex justify-content-center align-items-center" style="width:100%; margin-bottom:10px;"><a href="<?php echo site_url(); ?>workharder/"><img src="https://www.smartads.in/resources/assets/images/logo/smartadslogo.png" alt=""></a></div>
+        <div class="form-group">
+
+                <input type="text" class="form-control" name="loginUsername" placeholder="Nazwa użytkownika albo adres email" required>
+
+               </div>
+               <div class="form-group">
+               <input type="password" class="form-control" name="loginPassword" placeholder="Hasło" required> </div>
+               
+            <div class="form-row"><div class="form-group col-md-12 d-flex align-items-center justify-content-end"><button  type="submit" class="btn btn-primary btn-lg" style="width:100%;">Zaloguj się</button></div></div><div class="form-row"><div class="form-group col-md-12" style="margin-bottom:0; text-align:center;"><a href="#pomocprzyhasle" style="font-size:12px; border-right-style: solid; padding-right:10px; border-right-width:thin;">Zapomniałem hasła</a><a href="#pomocprzyemailu" style="font-size:12px; padding-left:10px;">e-maila</a></div></div>
+        </form></div>
+        <div class="register-prop col-7 container-fluid"><div class="row"><div class="col-12" style="text-align:center;"><h2>Nie masz jeszcze konta?</h2></div></div><div class="row"><div class="col-12" style="text-align:center;"><h5>Dołącz już dzisiaj do naszej społeczności aby pokonać prokrastynację!</h5></div></div><div class="row" ><div class="d-flex align-items-center justify-content-center" style="width:100%; padding-top:20px;"><button type="button" class="btn btn-primary btn-lg">Zarejestruj się!</button></div></div></div><div style="clear:both;"></div>
+        
+        </div> <!--   Okno logowania KONIEC    --> </div>
+    </div>
+      <div class="row navbar" style="margin:0">
         <div class="col-2" ><a href="<?php echo site_url(); ?>workharder/"><img src="https://www.smartads.in/resources/assets/images/logo/smartadslogo.png" alt=""></a></div>
         
           <div class="col-1"></div>
@@ -37,10 +80,10 @@ if($isRegister) {
   <a class="nav-link" href="#">Wspomóż projekt</a>
   
 </nav></div>
-              <div class="col-2"><div class="d-flex justify-content-end"><button type="button" class="btn btn-primary">Zaloguj się</button></div></div>
+              <div class="col-2"><div class="d-flex justify-content-end"><button type="button" class="btn btn-primary btn-login">Zaloguj się</button></div></div>
              
     </div> 
-       <div class="row" style="height:100%;" ><div class="container-fluid x" style="height:100%;"><div class="col-12" style="height:100%;" ><div class="d-flex justify-content-center align-items-center" style="height:100%;"><div class="bordersquare">
+       <div class="row" style="height:100%;width:100%; margin:0" ><div class="container-fluid x" style="height:100%;width:100%;"><div class="col-12" style="height:100%;width:100%;" ><div class="d-flex justify-content-center align-items-center" style="height:100%;"><div class="bordersquare">
           <div class="field-form d-flex align-items-center justify-content-center">
           <form action="http://[::1]/codeigniter/workharder/register" method="post" accept-charset="utf-8" style="width:100%;">
                <div class="form-group">
